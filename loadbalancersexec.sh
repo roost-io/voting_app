12c8df566d233a044d9709d41eee2720c4ad3b06
VIVEK K B <kbvivek@zb.io>
2022-12-28 14:16:33 +0530

    ZBIO-4000 Small fixes

--- loadbalancersexec.sh
+++ loadbalancersexec.sh
@@ -13,14 +13,14 @@ sleep 5
 
 ecserver_loadbalancer=$(kubectl get svc -n $ROOST_NAMESPACE | grep my-ecserver | awk '{print $4}')
 
-sed -i -e "s#REACT_APP_BALLOT_ENDPOINT_VALUE#${ballot_loadbalancer}#" -e "s#REACT_APP_EC_SERVER_ENDPOINT_VALUE#${ecserver_loadbalancer}#" voter/voter.yaml
+sed -i -e "s#REACT_APP_BALLOT_ENDPOINT_VALUE#${ballot_loadbalancer}8080#" -e "s#REACT_APP_EC_SERVER_ENDPOINT_VALUE#${ecserver_loadbalancer}8081#" voter/voter.yaml
 
 kubectl apply -f voter/voter.yaml -n $ROOST_NAMESPACE
 kubectl expose deployment voter --type=LoadBalancer --name=my-voter -n $ROOST_NAMESPACE
 
 sleep 5
 
-sed -i -e "s#EC_SERVER_ENDPOINT_VALUE#${ecserver_loadbalancer}#" election-commission/ec.yaml
+sed -i -e "s#EC_SERVER_ENDPOINT_VALUE#${ecserver_loadbalancer}8081#" election-commission/ec.yaml
 
 kubectl apply -f election-commission/ec.yaml -n $ROOST_NAMESPACE
 kubectl expose deployment election-commission --type=LoadBalancer --name=my-ec -n $ROOST_NAMESPACE
