abaff3bfd32cf8474e9c1ff2d95a676ed405d624
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-03-09 19:28:26 +0530

    Fixing get and post call

--- voter/pages/api/count.js
+++ voter/pages/api/count.js
@@ -5,6 +5,7 @@ export default function handler(req, res) {
     if (!/^(?:f|ht)tps?\:\/\//.test(ballot_endpoint)) {
       ballot_endpoint = "http://" + ballot_endpoint;
       }
+    if(req.method === 'POST'){
     fetch(`${ballot_endpoint}`, {
       method: 'POST',
       body: JSON.stringify(JSON.parse(req.body))
@@ -19,6 +20,21 @@ export default function handler(req, res) {
         );
         res.status(400).json({ sucess:false })
       });
+    }else {
+      fetch(`${ballot_endpoint}`, {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          res.status(200).json(response)
+        })
+        .catch((error) => {
+          console.error(
+            'ballot service is not reachable at http://' + ballot_endpoint
+          );
+          res.status(400).json({  })
+        });
+    }
 
     
   
