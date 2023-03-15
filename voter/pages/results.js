b49cf03256790ff8d559ff70bcc19fdaa26c1e92
ishanrai13 <ishanbansal012@gmail.com>
2023-03-15 15:23:17 +0530

    ZBIO-4196 voting_app results list bug

--- voter/pages/results.js
+++ voter/pages/results.js
@@ -7,7 +7,6 @@ import React, { Component } from 'react';
 import kubernates from '../public/assets/kubernates.png';
 // import './App.css';
 
-
 class Result extends Component {
 	constructor(props) {
 		super(props);
@@ -31,25 +30,33 @@ class Result extends Component {
 			.then((response) => {
 				// console.log('In resposnse: ', response);
 				let date = new Date();
-				this.setState({ results: response.results });
-				this.setState({ total_votes: response.total_votes });
+				const results = response.results;
+				fetch(`/api/candidate`, {
+					method: 'GET',
+				})
+					.then((response) => response.json())
+					.then((response) => {
+						let total = 0;
+						const filteredResults = results.filter((result) => {
+							return response.Candidates.some((e) => {
+								if (e.Name === result.candidate_id) {
+									total += result.vote_count;
+								}
+								return e.Name === result.candidate_id;
+							});
+						});
+						this.setState({ candidates: response.Candidates });
+						this.setState({ results: filteredResults });
+						this.setState({ total_votes: total });
+					})
+					.catch((error) => {
+						console.error('ballot service is not reachable at http://');
+					});
 			})
 			.catch((error) => {
 				console.error('error getting ballot results: ', error);
 				this.setState({ results: [] });
 				this.setState({ total_votes: 0 });
-			});
-		fetch(`/api/candidate`, {
-			method: 'GET',
-		})
-			.then((response) => response.json())
-			.then((response) => {
-				this.setState({ candidates: response.Candidates });
-			})
-			.catch((error) => {
-				console.error(
-					'ballot service is not reachable at http://' 
-				);
 			});
 	}
 
@@ -61,18 +68,20 @@ class Result extends Component {
 					<div className="cardBackgroundContainer">
 						<div className="cardBackground"></div>
 						<div className="cardBackgroundImage">
-							{this.state.candidates.map((c,i) => {
+							{this.state.candidates.map((c, i) => {
 								if (c.Name === candidate.candidate_id) {
-									return <img
-										src={c.ImageUrl}
-										width="150px"
-										height="150px"
-										className="image"
-										alt={c.Name}
-										key={i}
-									/>;
+									return (
+										<img
+											src={c.ImageUrl}
+											width="150px"
+											height="150px"
+											className="image"
+											alt={c.Name}
+											key={i}
+										/>
+									);
 								}
-								return <div key={i}></div>
+								return <div key={i}></div>;
 							})}
 							{/* {candidate.candidate_id === 'roost' ? (
 								<img
@@ -123,9 +132,10 @@ class Result extends Component {
 								}}
 							></div>
 							<div>
-								{Math.round(
-									(candidate.vote_count / this.state.total_votes) * 100
-								)}
+								{(
+									(candidate.vote_count / this.state.total_votes) *
+									100
+								).toFixed(2)}
 								%
 							</div>
 						</div>
@@ -143,7 +153,7 @@ class Result extends Component {
 		return (
 			<div className="Home">
 				<div className="logo">
-					<img src={kubernates.src} width="70px" height="70px" alt={"logo"}/>
+					<img src={kubernates.src} width="70px" height="70px" alt={'logo'} />
 				</div>
 				<div className="heading">
 					Developers preference for building K8S cluster, as of{' '}
